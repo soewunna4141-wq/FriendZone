680c4b9c4798d86af80e96aa64a490862940952f
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 22:47:52 +0630

    Update Friend.kt

--- app/src/main/java/com/fz/friendzone/core/model/Friend.kt
+++ app/src/main/java/com/fz/friendzone/core/model/Friend.kt
@@ -1,1 +1,8 @@
+package com.fz.friendzone.core.model
 
+data class Friend(
+    val id: String,
+    val userId: String,
+    val friendUserId: String,
+    val createdAt: Long = System.currentTimeMillis()
+)
