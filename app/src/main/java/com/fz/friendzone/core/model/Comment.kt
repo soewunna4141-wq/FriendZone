597bcb699b25514e4a4de60b25c13019d089bf35
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 22:50:35 +0630

    Update Comment.kt

--- app/src/main/java/com/fz/friendzone/core/model/Comment.kt
+++ app/src/main/java/com/fz/friendzone/core/model/Comment.kt
@@ -1,1 +1,9 @@
+package com.fz.friendzone.core.model
 
+data class Comment(
+    val id: String,
+    val postId: String,
+    val userId: String,
+    val text: String,
+    val createdAt: Long = System.currentTimeMillis()
+)
