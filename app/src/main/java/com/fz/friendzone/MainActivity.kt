dde9beff5c6e53c406dec1641377761661806f18
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 15:47:18 +0630

    Update MainActivity.kt

--- app/src/main/java/com/fz/friendzone/MainActivity.kt
+++ app/src/main/java/com/fz/friendzone/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    ComposerScreen()
+                    FriendZoneNavHost()
                 }
             }
         }
