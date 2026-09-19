f8d5f250624acc7191057cc231b10f77061aa92b
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 16:14:33 +0630

    Update FriendZoneNavHost.kt

--- app/src/main/java/com/fz/friendzone/FriendZoneNavHost.kt
+++ app/src/main/java/com/fz/friendzone/FriendZoneNavHost.kt
@@ -5,17 +5,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-private const val ROUTE_COMPOSER = "composer"
-
 @Composable
 fun FriendZoneNavHost() {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = ROUTE_COMPOSER
+        startDestination = NavigationRoutes.COMPOSER
     ) {
-        composable(ROUTE_COMPOSER) {
+        composable(NavigationRoutes.COMPOSER) {
             ComposerScreen()
         }
     }
