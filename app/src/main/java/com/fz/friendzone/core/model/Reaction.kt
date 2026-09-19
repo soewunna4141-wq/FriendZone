575379f12517e1b77c12d0d280e2857475dee65a
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 22:53:43 +0630

    Update Reaction.kt

--- app/src/main/java/com/fz/friendzone/core/model/Reaction.kt
+++ app/src/main/java/com/fz/friendzone/core/model/Reaction.kt
@@ -1,1 +1,9 @@
+package com.fz.friendzone.core.model
 
+data class Reaction(
+    val id: String,
+    val postId: String,
+    val userId: String,
+    val type: String = "LIKE",
+    val createdAt: Long = System.currentTimeMillis()
+)
