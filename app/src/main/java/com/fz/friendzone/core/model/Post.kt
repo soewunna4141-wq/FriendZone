eda5893b1435bb2fa227c8029692d3e0134a91de
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 22:45:19 +0630

    Update Post.kt

--- app/src/main/java/com/fz/friendzone/core/model/Post.kt
+++ app/src/main/java/com/fz/friendzone/core/model/Post.kt
@@ -1,1 +1,9 @@
+package com.fz.friendzone.core.model
 
+data class Post(
+    val id: String,
+    val userId: String,
+    val caption: String,
+    val mediaUrl: String? = null,
+    val createdAt: Long = System.currentTimeMillis()
+)
