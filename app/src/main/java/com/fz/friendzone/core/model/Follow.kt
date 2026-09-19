f8c93f2abc534930a0c5d432d6e5f9dafa592f29
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 22:56:19 +0630

    Update Follow.kt

--- app/src/main/java/com/fz/friendzone/core/model/Follow.kt
+++ app/src/main/java/com/fz/friendzone/core/model/Follow.kt
@@ -1,1 +1,8 @@
+package com.fz.friendzone.core.model
 
+data class Follow(
+    val id: String,
+    val followerId: String,
+    val followingId: String,
+    val createdAt: Long = System.currentTimeMillis()
+)
