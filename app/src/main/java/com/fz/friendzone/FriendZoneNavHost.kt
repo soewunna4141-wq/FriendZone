b14804c5d208b7dd20a0e6e75b7a578644d15bfd
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-19 21:41:36 +0630

    Update FriendZoneNavHost.kt

--- app/src/main/java/com/fz/friendzone/FriendZoneNavHost.kt
+++ app/src/main/java/com/fz/friendzone/FriendZoneNavHost.kt
@@ -5,7 +5,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.fz.friendzone.ComposerScreen
-import com.fz.friendzone.NavigationRoutes
 
 @Composable
 fun FriendZoneNavHost() {
