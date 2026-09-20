093ee8bd644a2ff408e3f70edd56a420aaad3d71
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-20 21:52:10 +0630

    Create AccountScreen.kt
    Add account screen foundation

---
+++ app/src/main/java/com/fz/friendzone/feature/account/AccountScreen.kt
@@ -0,0 +1,9 @@
+package com.fz.friendzone.feature.account
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AccountScreen() {
+    Text(text = "Account")
+}
