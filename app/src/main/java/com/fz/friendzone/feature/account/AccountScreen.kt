6f76d9ca4aef71a37a3f628ae0630fdf668b99a2
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 09:24:48 +0630

    Update AccountScreen.kt
    Connect account screen to ViewModel

--- app/src/main/java/com/fz/friendzone/feature/account/AccountScreen.kt
+++ app/src/main/java/com/fz/friendzone/feature/account/AccountScreen.kt
@@ -2,8 +2,17 @@ package com.fz.friendzone.feature.account
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.fz.friendzone.data.repository.AccountRepositoryFactory
 
 @Composable
 fun AccountScreen() {
+
+    val repository = AccountRepositoryFactory.create()
+
+    val viewModel: AccountViewModel = viewModel(
+        factory = AccountViewModelFactory(repository)
+    )
+
     Text(text = "Account")
 }
