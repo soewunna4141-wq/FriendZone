c0188f813650649b1d3b47c0fdde80a2c0579fc9
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 20:34:13 +0630

    Update NavigationRoutes.kt
    Add profile navigation route

--- app/src/main/java/com/fz/friendzone/core/navigation/NavigationRoutes.kt
+++ app/src/main/java/com/fz/friendzone/core/navigation/NavigationRoutes.kt
@@ -1,6 +1,10 @@
 package com.fz.friendzone.core.navigation
 
 object NavigationRoutes {
+
     const val COMPOSER = "composer"
+
     const val ACCOUNT = "account"
+
+    const val PROFILE = "profile"
 }
