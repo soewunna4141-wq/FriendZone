83ddefee807a96a98f2e2209d0131c67399cdfff
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 15:28:30 +0630

    Create FriendZoneApplication.kt
    Add FriendZone application composition root

---
+++ app/src/main/java/com/fz/friendzone/FriendZoneApplication.kt
@@ -0,0 +1,10 @@
+package com.fz.friendzone
+
+import android.app.Application
+import com.fz.friendzone.app.FriendZoneDependencies
+
+class FriendZoneApplication : Application() {
+
+    val dependencies: FriendZoneDependencies
+        get() = FriendZoneDependencies
+}
