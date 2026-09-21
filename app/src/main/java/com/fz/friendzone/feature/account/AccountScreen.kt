5d79574fb1bd2785d1f10b1105cc85ed27732e62
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 14:05:45 +0630

    Update AccountScreen.kt
    Connect account UI write operation to ViewModel

--- app/src/main/java/com/fz/friendzone/feature/account/AccountScreen.kt
+++ app/src/main/java/com/fz/friendzone/feature/account/AccountScreen.kt
@@ -1,10 +1,12 @@
 package com.fz.friendzone.feature.account
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.fz.friendzone.core.model.Account
 import com.fz.friendzone.data.repository.AccountRepositoryFactory
 
 @Composable
@@ -26,5 +28,18 @@ fun AccountScreen() {
         Text(
             text = "Account ID: ${account.id}\nActive: ${account.isActive}"
         )
+
+        Button(
+            onClick = {
+                viewModel.saveAccount(
+                    Account(
+                        id = account.id,
+                        isActive = !account.isActive
+                    )
+                )
+            }
+        ) {
+            Text(text = "Toggle Active")
+        }
     }
 }
