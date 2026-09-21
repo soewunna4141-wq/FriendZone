453e72f0da72a204f8d0b449605fc3e69b49f592
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 20:04:06 +0630

    Create ProfileViewModelFactory.kt
    Add profile ViewModel factory

---
+++ app/src/main/java/com/fz/friendzone/feature/profile/ProfileViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.fz.friendzone.feature.profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.fz.friendzone.data.repository.ProfileRepository
+
+class ProfileViewModelFactory(
+    private val repository: ProfileRepository
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
+            return ProfileViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException(
+            "Unknown ViewModel class: ${modelClass.name}"
+        )
+    }
+}
