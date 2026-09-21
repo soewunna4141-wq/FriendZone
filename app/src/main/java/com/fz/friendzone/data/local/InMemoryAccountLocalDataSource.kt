a4d7ce6f7d43d8b2919a87c79ac71e004fefdc75
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 09:38:32 +0630

    Update InMemoryAccountLocalDataSource.kt
    Add demo account data

--- app/src/main/java/com/fz/friendzone/data/local/InMemoryAccountLocalDataSource.kt
+++ app/src/main/java/com/fz/friendzone/data/local/InMemoryAccountLocalDataSource.kt
@@ -4,7 +4,10 @@ import com.fz.friendzone.core.model.Account
 
 class InMemoryAccountLocalDataSource : AccountLocalDataSource {
 
-    private var account: Account? = null
+    private var account: Account? = Account(
+        id = "demo-account",
+        isActive = true
+    )
 
     override fun getAccount(): Account? {
         return account
