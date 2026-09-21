d2e6645cbbd79615eaad8157db4cab815879ce4d
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 09:48:16 +0630

    Update AccountViewModel.kt
    Add account UI state foundation

--- app/src/main/java/com/fz/friendzone/feature/account/AccountViewModel.kt
+++ app/src/main/java/com/fz/friendzone/feature/account/AccountViewModel.kt
@@ -4,11 +4,21 @@ import androidx.lifecycle.ViewModel
 import com.fz.friendzone.core.model.Account
 import com.fz.friendzone.data.repository.AccountRepository
 
+data class AccountUiState(
+    val account: Account? = null
+)
+
 class AccountViewModel(
     private val repository: AccountRepository
 ) : ViewModel() {
 
     fun getAccount(): Account? {
         return repository.getAccount()
+    }
+
+    fun getUiState(): AccountUiState {
+        return AccountUiState(
+            account = getAccount()
+        )
     }
 }
