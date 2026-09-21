c67f3bac5384fca11ccf3b2f0b80ce2467f90b32
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-21 14:28:12 +0630

    Update AccountViewModelFactory.kt
    Move account repository creation into ViewModel factory

--- app/src/main/java/com/fz/friendzone/feature/account/AccountViewModelFactory.kt
+++ app/src/main/java/com/fz/friendzone/feature/account/AccountViewModelFactory.kt
@@ -2,11 +2,11 @@ package com.fz.friendzone.feature.account
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.fz.friendzone.data.repository.AccountRepository
+import com.fz.friendzone.data.repository.AccountRepositoryFactory
 
-class AccountViewModelFactory(
-    private val repository: AccountRepository
-) : ViewModelProvider.Factory {
+class AccountViewModelFactory : ViewModelProvider.Factory {
+
+    private val repository = AccountRepositoryFactory.create()
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
