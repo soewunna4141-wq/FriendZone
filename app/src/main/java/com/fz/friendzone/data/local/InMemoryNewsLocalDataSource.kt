a5f980616e4d9d84932282386017a3c02eaab927
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-23 08:12:18 +0630

    Create InMemoryNewsLocalDataSource.kt
    Add in-memory News local data source

---
+++ app/src/main/java/com/fz/friendzone/data/local/InMemoryNewsLocalDataSource.kt
@@ -0,0 +1,12 @@
+package com.fz.friendzone.data.local
+
+import com.fz.friendzone.core.model.Post
+
+class InMemoryNewsLocalDataSource : NewsLocalDataSource {
+
+    private val posts = mutableListOf<Post>()
+
+    override fun getPosts(): List<Post> {
+        return posts.toList()
+    }
+}
