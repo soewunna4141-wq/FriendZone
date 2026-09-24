abd42738cc5e6d0b6afc467ac5a5110b65d28c2e
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-24 09:30:07 +0630

    Update FriendZoneDependencies.kt
    Add NewsRepository to application dependencies

--- app/src/main/java/com/fz/friendzone/app/FriendZoneDependencies.kt
+++ app/src/main/java/com/fz/friendzone/app/FriendZoneDependencies.kt
@@ -2,6 +2,8 @@ package com.fz.friendzone.app
 
 import com.fz.friendzone.data.repository.AccountRepository
 import com.fz.friendzone.data.repository.AccountRepositoryFactory
+import com.fz.friendzone.data.repository.NewsRepository
+import com.fz.friendzone.data.repository.NewsRepositoryFactory
 import com.fz.friendzone.data.repository.ProfileRepository
 import com.fz.friendzone.data.repository.ProfileRepositoryFactory
 import com.fz.friendzone.feature.account.AccountViewModelFactory
@@ -23,5 +25,9 @@ class FriendZoneDependencies {
 
     val profileViewModelFactory: ProfileViewModelFactory by lazy {
         ProfileViewModelFactory(profileRepository)
+    }
+
+    val newsRepository: NewsRepository by lazy {
+        NewsRepositoryFactory.create()
     }
 }
