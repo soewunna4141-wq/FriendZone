58cfb569d18bbd1860012d9b3ca20e20ef23b7d3
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-24 10:49:56 +0630

    Create NewsViewModelTest.kt
    Add NewsViewModel unit tests

---
+++ app/src/test/java/com/fz/friendzone/feature/news/NewsViewModelTest.kt
@@ -0,0 +1,57 @@
+package com.fz.friendzone.feature.news
+
+import com.fz.friendzone.core.model.Post
+import com.fz.friendzone.data.repository.NewsRepository
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NewsViewModelTest {
+
+    @Test
+    fun loadAction_updatesUiStateWithPosts() {
+        val posts = listOf(
+            Post(
+                id = "post-1",
+                userId = "user-1",
+                caption = "Test post"
+            )
+        )
+
+        val repository = object : NewsRepository {
+            override fun getPosts(): List<Post> {
+                return posts
+            }
+        }
+
+        val viewModel = NewsViewModel(
+            repository = repository
+        )
+
+        viewModel.onAction(NewsAction.Load)
+
+        assertEquals(
+            NewsUiState.Success(posts),
+            viewModel.uiState.value
+        )
+    }
+
+    @Test
+    fun loadAction_whenRepositoryFails_updatesUiStateWithError() {
+        val repository = object : NewsRepository {
+            override fun getPosts(): List<Post> {
+                throw IllegalStateException("Load failed")
+            }
+        }
+
+        val viewModel = NewsViewModel(
+            repository = repository
+        )
+
+        viewModel.onAction(NewsAction.Load)
+
+        assertEquals(
+            NewsUiState.Error("Load failed"),
+            viewModel.uiState.value
+        )
+    }
+}
