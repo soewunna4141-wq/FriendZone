5fb72c157d5eff657e68a42f8143e8ad940d5a77
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-25 15:49:47 +0630

    Create UserRepositoryImplTest.kt
    Add User repository tests

---
+++ app/src/test/java/com/fz/friendzone/data/repository/UserRepositoryImplTest.kt
@@ -0,0 +1,112 @@
+package com.fz.friendzone.data.repository
+
+import com.fz.friendzone.core.model.User
+import com.fz.friendzone.data.local.UserLocalDataSource
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class UserRepositoryImplTest {
+
+    @Test
+    fun getCurrentUser_returnsUserFromLocalDataSource() {
+        val user = User(
+            id = "user-1",
+            name = "Test User",
+            username = "testuser"
+        )
+
+        val localDataSource = object : UserLocalDataSource {
+
+            override fun getCurrentUser(): User? {
+                return user
+            }
+
+            override fun getUser(userId: String): User? {
+                return null
+            }
+        }
+
+        val repository = UserRepositoryImpl(
+            localDataSource = localDataSource
+        )
+
+        assertEquals(
+            user,
+            repository.getCurrentUser()
+        )
+    }
+
+    @Test
+    fun getCurrentUser_returnsNullWhenLocalDataSourceReturnsNull() {
+        val localDataSource = object : UserLocalDataSource {
+
+            override fun getCurrentUser(): User? {
+                return null
+            }
+
+            override fun getUser(userId: String): User? {
+                return null
+            }
+        }
+
+        val repository = UserRepositoryImpl(
+            localDataSource = localDataSource
+        )
+
+        assertNull(
+            repository.getCurrentUser()
+        )
+    }
+
+    @Test
+    fun getUser_returnsUserFromLocalDataSource() {
+        val user = User(
+            id = "user-1",
+            name = "Test User",
+            username = "testuser"
+        )
+
+        val localDataSource = object : UserLocalDataSource {
+
+            override fun getCurrentUser(): User? {
+                return null
+            }
+
+            override fun getUser(userId: String): User? {
+                return if (userId == user.id) user else null
+            }
+        }
+
+        val repository = UserRepositoryImpl(
+            localDataSource = localDataSource
+        )
+
+        assertEquals(
+            user,
+            repository.getUser("user-1")
+        )
+    }
+
+    @Test
+    fun getUser_returnsNullWhenLocalDataSourceReturnsNull() {
+        val localDataSource = object : UserLocalDataSource {
+
+            override fun getCurrentUser(): User? {
+                return null
+            }
+
+            override fun getUser(userId: String): User? {
+                return null
+            }
+        }
+
+        val repository = UserRepositoryImpl(
+            localDataSource = localDataSource
+        )
+
+        assertNull(
+            repository.getUser("user-1")
+        )
+    }
+}
