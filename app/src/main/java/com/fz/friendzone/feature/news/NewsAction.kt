f52b16b5e4119e0a9c012c15047fce5dcee980ed
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-25 14:07:46 +0630

    Update NewsAction.kt
    Add News create post action

--- app/src/main/java/com/fz/friendzone/feature/news/NewsAction.kt
+++ app/src/main/java/com/fz/friendzone/feature/news/NewsAction.kt
@@ -1,6 +1,12 @@
 package com.fz.friendzone.feature.news
 
+import com.fz.friendzone.core.model.Post
+
 sealed interface NewsAction {
 
     data object Load : NewsAction
+
+    data class CreatePost(
+        val post: Post
+    ) : NewsAction
 }
