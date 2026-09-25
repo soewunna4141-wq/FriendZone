344727e614893bbff77c3ae6618e152220c2cfea
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-25 08:15:18 +0630

    Update NewsUiState.kt
    Add News post presentation model

--- app/src/main/java/com/fz/friendzone/feature/news/NewsUiState.kt
+++ app/src/main/java/com/fz/friendzone/feature/news/NewsUiState.kt
@@ -2,16 +2,22 @@ package com.fz.friendzone.feature.news
 
 import androidx.annotation.StringRes
 import com.fz.friendzone.core.model.Post
+import com.fz.friendzone.core.model.Profile
 
 sealed interface NewsUiState {
 
     data object Loading : NewsUiState
 
     data class Success(
-        val posts: List<Post>
+        val posts: List<NewsPostUiModel>
     ) : NewsUiState
 
     data class Error(
         @StringRes val messageResId: Int
     ) : NewsUiState
 }
+
+data class NewsPostUiModel(
+    val post: Post,
+    val profile: Profile?
+)
