a0ceb64efbd8115a304f23994fc082ef83c1861b
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-25 14:32:54 +0630

    Create UserRepository.kt
    Add User repository identity boundary

---
+++ app/src/main/java/com/fz/friendzone/data/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.fz.friendzone.data.repository
+
+import com.fz.friendzone.core.model.User
+
+interface UserRepository {
+
+    fun getCurrentUser(): User?
+
+    fun getUser(userId: String): User?
+}
