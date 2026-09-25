c06f31a9b7dbbff8fb1d68ee560b33085b60511d
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-25 20:09:24 +0630

    Update InMemoryUserLocalDataSource.kt
    Update InMemoryUserLocalDataSource.kt
    
    Implement User write boundary

--- app/src/main/java/com/fz/friendzone/data/local/InMemoryUserLocalDataSource.kt
+++ app/src/main/java/com/fz/friendzone/data/local/InMemoryUserLocalDataSource.kt
@@ -2,11 +2,17 @@ package com.fz.friendzone.data.local
 
 import com.fz.friendzone.core.model.User
 
-class InMemoryUserLocalDataSource : UserLocalDataSource {
-
+class InMemoryUserLocalDataSource(
     private var currentUser: User? = null
+) : UserLocalDataSource {
 
     private val users = mutableMapOf<String, User>()
+
+    init {
+        currentUser?.let { user ->
+            users[user.id] = user
+        }
+    }
 
     override fun getCurrentUser(): User? {
         return currentUser
@@ -14,5 +20,9 @@ class InMemoryUserLocalDataSource : UserLocalDataSource {
 
     override fun getUser(userId: String): User? {
         return users[userId]
+    }
+
+    override fun saveUser(user: User) {
+        users[user.id] = user
     }
 }
