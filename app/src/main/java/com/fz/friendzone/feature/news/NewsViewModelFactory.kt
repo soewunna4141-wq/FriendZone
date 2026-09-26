9f021d2528c4e0185c2154e803e0061d960c7483
soewunna4141-wq <soewunna4141@gmail.com>
2026-09-26 21:51:59 +0630

    Update NewsViewModelFactory.kt
    Wire comment repository into news view model factory

--- app/src/main/java/com/fz/friendzone/feature/news/NewsViewModelFactory.kt
+++ app/src/main/java/com/fz/friendzone/feature/news/NewsViewModelFactory.kt
@@ -2,6 +2,7 @@ package com.fz.friendzone.feature.news
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.fz.friendzone.data.repository.CommentRepository
 import com.fz.friendzone.data.repository.NewsRepository
 import com.fz.friendzone.data.repository.ProfileRepository
 import com.fz.friendzone.data.repository.ReactionRepository
@@ -9,7 +10,8 @@ import com.fz.friendzone.data.repository.ReactionRepository
 class NewsViewModelFactory(
     private val newsRepository: NewsRepository,
     private val profileRepository: ProfileRepository,
-    private val reactionRepository: ReactionRepository
+    private val reactionRepository: ReactionRepository,
+    private val commentRepository: CommentRepository
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
@@ -19,7 +21,8 @@ class NewsViewModelFactory(
         return NewsViewModel(
             newsRepository = newsRepository,
             profileRepository = profileRepository,
-            reactionRepository = reactionRepository
+            reactionRepository = reactionRepository,
+            commentRepository = commentRepository
         ) as T
     }
 }
